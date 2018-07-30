e2367e68e1987e08e2596ddb48540e55b841dde6
Shoma Nishitateno <shoma416@gmail.com>
2018-07-30 17:19:55 +0900

    Fix: indent

--- src/index.ts
+++ src/index.ts
@@ -22,20 +22,20 @@ global.showDialog = function() {
 }
 
 global.getConfig = function(): Config {
-	const propertyService = UserPropertyServiceImpl()
+  const propertyService = UserPropertyServiceImpl()
   return getConfigFromProperty(propertyService)
 }
 
 global.execute = function(config: Config): string {
-	const propertyService = UserPropertyServiceImpl()
-	const httpClient = HttpClientImpl()
+  const propertyService = UserPropertyServiceImpl()
+  const httpClient = HttpClientImpl()
 
   // フォームに入力された値を保存し、次回から入力しなくてもいいようにする
   storeConfig(config, propertyService)
 
   // プロジェクトユーザー一覧を取得する
-	const uri = `${config.url}/api/v2/projects/${config.projectKey}/users?apiKey=${config.apiKey}`
-	const usersJson = httpClient.get(uri)
+  const uri = `${config.url}/api/v2/projects/${config.projectKey}/users?apiKey=${config.apiKey}`
+  const usersJson = httpClient.get(uri)
   const users = Object.keys(usersJson).map(key => jsonToUser(usersJson[key]))
 
   // スプレッドシートに新たなシートを追加する
@@ -63,39 +63,39 @@ global.execute = function(config: Config): string {
 }
 
 export const jsonToUser = (json: any): User =>
-	User(
-		json["id"],
-		json["userId"],
-		json["name"],
-		json["mailAddress"]
-	)
+  User(
+    json["id"],
+    json["userId"],
+    json["name"],
+    json["mailAddress"]
+  )
 
 const getConfigFromProperty = (propertyService: UserPropertyService): Config =>
-	Config(
-		propertyService.get('url'),
-		propertyService.get('apiKey'),
-		propertyService.get('projectKey')	
-	)
+  Config(
+    propertyService.get('url'),
+    propertyService.get('apiKey'),
+    propertyService.get('projectKey')	
+  )
 
 const storeConfig = (config: Config, propertyService: UserPropertyService): void => {
-	propertyService.set('url', config.url)
-	propertyService.set('apiKey', config.apiKey)
-	propertyService.set('projectKey', config.projectKey)
+  propertyService.set('url', config.url)
+  propertyService.set('apiKey', config.apiKey)
+  propertyService.set('projectKey', config.projectKey)
 }
 
 interface Config {
   readonly url: string
-	readonly apiKey: string
-	readonly projectKey: string
+  readonly apiKey: string
+  readonly projectKey: string
 }
 
 const Config = (url: string, apiKey: string, projectKey: string) => ({url, apiKey, projectKey})
 
 export interface User {
-	readonly id: number
-	readonly userId: string
-	readonly name: string
-	readonly mailAddress: string
+  readonly id: number
+  readonly userId: string
+  readonly name: string
+  readonly mailAddress: string
 }
 
 export const User = (id: number, userId: string, name: string, mailAddress: string) => ({id, userId, name, mailAddress}) 
