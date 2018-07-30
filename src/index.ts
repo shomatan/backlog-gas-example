aeb47dff925268eed1fb28997c959f88e6775f4f
Shoma Nishitateno <shoma416@gmail.com>
2018-07-30 13:32:06 +0900

    Update: output users

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ global.getConfig = function(): Config {
 	return getConfig()
 }
 
-global.execute = function(config: Config): void {
+global.execute = function(config: Config): string {
 	// フォームに入力された値を保存し、次回から入力しなくてもいいようにする
 	storeConfig(config)
 
@@ -33,7 +33,29 @@ global.execute = function(config: Config): void {
 	const usersJson = httpGet(`${config.url}/api/v2/projects/${config.projectKey}/users?apiKey=${config.apiKey}`)
 	const users = Object.keys(usersJson).map(key => jsonToUser(usersJson[key]))
 
-	throw new Error(users[0].name)
+	// スプレッドシートに新たなシートを追加する
+	const sheetName = Utilities.formatDate(new Date(), "JST", "yyyy/MM/dd HH:mm:ss")
+	const sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(sheetName, 0)
+
+	// ヘッダーを設定
+	sheet.getRange(1, 1).setValue("ID")
+	sheet.getRange(1, 2).setValue("ユーザーID")
+	sheet.getRange(1, 3).setValue("名前")
+	sheet.getRange(1, 4).setValue("メールアドレス")
+
+	// データを出力
+	for (let i = 0; i < users.length; i++) {
+		const rowNumber = i + 2
+		const user = users[i]
+
+		sheet.getRange(rowNumber, 1).setValue(user.id)
+		sheet.getRange(rowNumber, 2).setValue(user.userId)
+		sheet.getRange(rowNumber, 3).setValue(user.name)
+		sheet.getRange(rowNumber, 4).setValue(user.mailAddress)
+		SpreadsheetApp.flush()
+	}
+
+	return "success"
 }
 
 const jsonToUser = (json: any): User =>
