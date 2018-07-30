f334c4c33700dcd0c2bcbe6ff24baec9e770b756
Shoma Nishitateno <shoma416@gmail.com>
2018-07-30 14:01:29 +0900

    Clean:

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ global.showDialog = function() {
 		
 	SpreadsheetApp
 		.getUi()
-		.showModalDialog(html, 'Backlogから自分の課題を取得するサンプルアプリケーション')
+		.showModalDialog(html, 'Backlogからプロジェクトユーザーを取得するサンプルアプリケーション')
 }
 
 global.getConfig = function(): Config {
@@ -26,35 +26,34 @@ global.getConfig = function(): Config {
 }
 
 global.execute = function(config: Config): string {
-	// フォームに入力された値を保存し、次回から入力しなくてもいいようにする
-	storeConfig(config)
+  // フォームに入力された値を保存し、次回から入力しなくてもいいようにする
+  storeConfig(config)
 
-	// プロジェクトユーザー一覧を取得する
-	const usersJson = httpGet(`${config.url}/api/v2/projects/${config.projectKey}/users?apiKey=${config.apiKey}`)
-	const users = Object.keys(usersJson).map(key => jsonToUser(usersJson[key]))
+  // プロジェクトユーザー一覧を取得する
+  const usersJson = httpGet(`${config.url}/api/v2/projects/${config.projectKey}/users?apiKey=${config.apiKey}`)
+  const users = Object.keys(usersJson).map(key => jsonToUser(usersJson[key]))
 
-	// スプレッドシートに新たなシートを追加する
-	const sheetName = Utilities.formatDate(new Date(), "JST", "yyyy/MM/dd HH:mm:ss")
-	const sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(sheetName, 0)
+  // スプレッドシートに新たなシートを追加する
+  const sheetName = Utilities.formatDate(new Date(), "JST", "yyyy/MM/dd HH:mm:ss")
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(sheetName, 0)
 
-	// ヘッダーを設定
-	sheet.getRange(1, 1).setValue("ID")
-	sheet.getRange(1, 2).setValue("ユーザーID")
-	sheet.getRange(1, 3).setValue("名前")
-	sheet.getRange(1, 4).setValue("メールアドレス")
+  // ヘッダーを設定
+  sheet.getRange(1, 1).setValue("ID")
+  sheet.getRange(1, 2).setValue("ユーザーID")
+  sheet.getRange(1, 3).setValue("名前")
+  sheet.getRange(1, 4).setValue("メールアドレス")
 
-	// データを出力
-	for (let i = 0; i < users.length; i++) {
-		const rowNumber = i + 2
-		const user = users[i]
+  // データを出力
+  for (let i = 0; i < users.length; i++) {
+    const rowNumber = i + 2
+    const user = users[i]
 
-		sheet.getRange(rowNumber, 1).setValue(user.id)
-		sheet.getRange(rowNumber, 2).setValue(user.userId)
-		sheet.getRange(rowNumber, 3).setValue(user.name)
-		sheet.getRange(rowNumber, 4).setValue(user.mailAddress)
-		SpreadsheetApp.flush()
-	}
-
+    sheet.getRange(rowNumber, 1).setValue(user.id)
+    sheet.getRange(rowNumber, 2).setValue(user.userId)
+    sheet.getRange(rowNumber, 3).setValue(user.name)
+    sheet.getRange(rowNumber, 4).setValue(user.mailAddress)
+    SpreadsheetApp.flush()
+  }
 	return "success"
 }
 
